10e4f67640323d0b98496d48a0ac367036440b3c
Pradeep <pradeep23oct@gmail.com>
2022-09-02 04:19:41 +0530

    style updated

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Result = ({ result, playClicked }) => {
   return (
     <div className='jspond-result'>
-      <div className='jspond-result-header'>Result: ({playClicked})</div>
+      <div className='jspond-result-header'>Output: ({playClicked})</div>
       <div className='jspond-result-content'>
         <pre>{result}</pre>
       </div>
